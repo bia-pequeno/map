ac3d67d414615a870ecca809eabe98f683d17bba
bia-pequeno <bia.2017pequeno@gmail.com>
2022-02-12 14:24:58 -0300

    concluido

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+class App extends Component{
+  
+  state = {
+    pessoas:[
+      {
+        nome: "Jason",
+        idade: 16
+      },
+      {
+        nome: "Piper",
+        idade: 16
+      },
+      {
+        nome: "Leo",
+        idade: 15
+      },
+      {
+        nome: "Percy",
+        idade: 16
+      },
+      {
+        nome: "Annebeth",
+        idade: 16
+      },
+      {
+        nome: "Nico",
+        idade: 12
+      }
+    ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    return(
+      <main>
+        {this.state.pessoas.map((item) => (
+          <div className="container">
+            <h2>Nome: {item.nome}</h2>
+            <p>idade: {item.idade}</p>
+          </div>
+        ))}
+      </main>
+    )
+  }
 }
 
-export default App;
+export default App
